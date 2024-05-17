34138642b193cb260905385f9795e4f6adbd7083
rwin96 <aryaei1383@gmail.com>
2024-05-17 15:37:40 +0330

    Add Util class

---
+++ src/main/java/org/example/Util.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import enums.SType;
+
+public class Util {
+
+    private static int index;
+    private static String input;
+
+    public static boolean isAccepted(Automata automata, String input) {
+
+        Util.index = 0;
+        Util.input = input;
+
+        State currentState = automata.getInitialState();
+        Transition thisTransition;
+
+        while (Util.index < input.length()) {
+            thisTransition = automata.findTransition(lookahead(), currentState);
+
+            if (thisTransition == null)
+                return false;
+            else {
+                currentState = thisTransition.getDestination();
+                Util.index++;
+            }
+        }
+
+        return currentState.getType() == SType.FINAL;
+    }
+
+    private static Character lookahead() {
+        return Util.input.charAt(Util.index);
+    }
+}
+
+
